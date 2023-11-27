63847b3ecf0cd77e73b43946be34ec3abdefdce5
Aleksandr Vinokurov <johannblx@gmail.com>
2023-11-27 16:22:55 +0700

    login
    trying to correctly fetch with api
    added basic HTML code

---
+++ fetch.js
@@ -0,0 +1,36 @@
+const requestURL = 'https://blog.kreosoft.space/api/account/login'
+
+function sendRequest(method, url, body = null) {
+  const headers = {
+    'Content-Type': 'application/json'
+  }
+
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: headers
+  }).then(response => {
+    if (response.ok) {
+      return response.json()
+    }
+
+    return response.json().then(error => {
+      const e = new Error('Что-то пошло не так')
+      e.data = error
+      throw e
+    })
+  })
+}
+
+/*sendRequest('GET', requestURL)
+  .then(data => console.log(data))
+  .catch(err => console.log(err))*/
+
+const body = {
+  email: 'abc@example.com',
+  password: 'string1'
+}
+
+sendRequest('POST', requestURL, body)
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
