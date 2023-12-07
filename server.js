e574c45628f1a6d6152d6182a75642afdecdf48a
Aleksandr Vinokurov <johannblx@gmail.com>
2023-12-07 18:12:00 +0700

    architecture modified
    routing added
    SPA added
    folders ordered
    scipts ordered

--- server.js
+++ server.js
@@ -1,40 +1,34 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = 5500;
 
-// Разрешить статические файлы из папки view
-app.use(express.static(path.join(__dirname, 'view')));
-//app.use('/login', express.static(path.join(__dirname, 'view')));
-app.use(express.static(path.join(__dirname, 'view')));
-//app.use('/login', express.static(path.join(__dirname, 'view')));
+const _dirname = __dirname + '/WebFront-Hits-frontend-project-2';
 
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'login.html'));
+app.get('/:page', (req, res) => {   
+    const page = req.params.page;
+  
+    // Отправляем базовую структуру
+    const indexPath = path.join(_dirname, '../index.html');
+    const indexContent = fs.readFileSync(indexPath, 'utf-8');
+  
+    // Читаем содержимое страницы и вставляем в <main>
+    const pagePath = path.join(_dirname, `../views/${page}.html`);
+    const pageContent = fs.readFileSync(pagePath, 'utf-8');
+  
+    // Вставляем содержимое страницы внутрь <main>
+    const modifiedContent = indexContent.replace('<main></main>', `<main>${pageContent}</main>`);
+  
+    // Отправляем результат
+    res.status(200).send(modifiedContent);
 });
-
-app.get('/scripts/loginFetch.js', (req, res) => {
-    res.sendFile(path.join(__dirname, 'scripts', 'loginFetch.js'));
-});
-
-app.use(express.static(path.join(__dirname, 'view')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'index.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'login.html'));
-});
-
-app.get('/registration', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'registration.html'));
-});
-app.get('/registration', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'registration.html'));
+  res.sendFile(path.join(_dirname, '../index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
